f78100e5a936d90bf7cf5ea843a5963a395d8d71
PLATEAUU <plateauu@o2.pl>
2016-05-25 21:41:06 +0200

    add TimerMain

--- src/pl/plateauu/Timer.java
+++ src/pl/plateauu/Timer.java
@@ -1,33 +1,22 @@
 package pl.plateauu;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class Timer
 {
 
 	private long startTime;
 	private long stopTime; 
 	
-	
-	public static void main(String[] args)
-	{
-		Timer gui = new Timer();
-		gui.start();
-		System.out.println(gui.startTime);
-		gui.stop();
-		System.out.println(gui.stopTime);
-
-	}
-
-	private void initGui()
-	{
-
-	}
-
-	private void start()
+	void start()
 	{
 		startTime = System.nanoTime();
 	}
 
-	private void stop()
+	void stop()
 	{
 		stopTime = System.nanoTime();
 		System.out.println(stopTime - startTime * 1e-9 );
