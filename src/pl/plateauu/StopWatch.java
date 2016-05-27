85114c258739892186d8c2f63cee0953214b0c8b
PLATEAUU <plateauu@o2.pl>
2016-05-27 23:47:11 +0200

    added MenuBar

--- src/pl/plateauu/StopWatch.java
+++ src/pl/plateauu/StopWatch.java
@@ -1,12 +1,84 @@
 package pl.plateauu;
 
-class StopWatch
+import java.io.Serializable;
+
+class StopWatch implements Serializable
 {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7224616195898281026L;
 	private long startTime = 0;
 	private long stopTime = 0;
 	private long resumeTime = 0;
 	private boolean running = false;
+	
+	
+
+	/**
+	 * @return the startTime
+	 */
+	public long getStartTime()
+	{
+		return startTime;
+	}
+
+	/**
+	 * @param startTime the startTime to set
+	 */
+	public void setStartTime(long startTime)
+	{
+		this.startTime = startTime;
+	}
+
+	/**
+	 * @return the stopTime
+	 */
+	public long getStopTime()
+	{
+		return stopTime;
+	}
+
+	/**
+	 * @param stopTime the stopTime to set
+	 */
+	public void setStopTime(long stopTime)
+	{
+		this.stopTime = stopTime;
+	}
+
+	/**
+	 * @return the resumeTime
+	 */
+	public long getResumeTime()
+	{
+		return resumeTime;
+	}
+
+	/**
+	 * @param resumeTime the resumeTime to set
+	 */
+	public void setResumeTime(long resumeTime)
+	{
+		this.resumeTime = resumeTime;
+	}
+
+	/**
+	 * @return the running
+	 */
+	public boolean isRunning()
+	{
+		return running;
+	}
+
+	/**
+	 * @param running the running to set
+	 */
+	public void setRunning(boolean running)
+	{
+		this.running = running;
+	}
 
 	public void start()
 	{
@@ -16,13 +88,16 @@ class StopWatch
 
 	public void stop()
 	{
-		this.stopTime = System.currentTimeMillis();
-		this.running = false;
+		if (running)
+		{
+			this.stopTime = System.currentTimeMillis();
+			this.running = false;
+		}
 	}
 
 	public void resume()
 	{
-		//this.stopTime = 0;
+		// this.stopTime = 0;
 		this.resumeTime = System.currentTimeMillis() - this.stopTime;
 		this.running = true;
 	}
--- src/pl/plateauu/StopWatchApp.java
+++ src/pl/plateauu/StopWatchApp.java
@@ -4,18 +4,28 @@ import java.awt.BorderLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
 public class StopWatchApp extends JFrame
 {
 
-	private StopWatch timer;
-	private JLabel timerField;
+	StopWatch timer;
+	JLabel timerField;
 	private String elapsedTimeSecs = "0";
 	private String elapsedTimeMinutes = "0";
 	private String elapsedTimeHours = "0";
@@ -51,6 +61,31 @@ public class StopWatchApp extends JFrame
 		stopButton.addActionListener(new StoptButtonListener());
 		JButton resumeButton = new JButton("WZNÓW");
 		resumeButton.addActionListener(new ResumeButtonListener());
+
+		// tutaj trzeba dorobiæ
+		JMenuBar menuBar = new JMenuBar();
+		JMenu fileMenu = new JMenu("File");
+		fileMenu.setMnemonic(KeyEvent.VK_F);
+
+		JMenuItem loadItem = new JMenuItem("Load");
+		loadItem.setMnemonic(KeyEvent.VK_L);
+		loadItem.addActionListener(new LoadItemListener());
+
+		JMenuItem saveItem = new JMenuItem("Save");
+		saveItem.setMnemonic(KeyEvent.VK_S);
+		saveItem.addActionListener(new SaveItemListener());
+
+		JMenuItem exitItem = new JMenuItem("Exit");
+		exitItem.setMnemonic(KeyEvent.VK_E);
+		exitItem.addActionListener(new ExitItemListener());
+
+		fileMenu.add(saveItem);
+		fileMenu.add(loadItem);
+		fileMenu.addSeparator();
+		fileMenu.add(exitItem);
+
+		menuBar.add(fileMenu);
+		this.setJMenuBar(menuBar);
 
 		panel.add(timerLabel);
 		panel.add(timerField);
@@ -99,13 +134,7 @@ public class StopWatchApp extends JFrame
 		@Override
 		public void actionPerformed(ActionEvent arg0)
 		{
-			String elapsedTime = Long.toString(timer.getElapsedTimeSecs());
-			elapsedTimeSecs = Long.toString(timer.getElapsedTimeSecs() % 60);
-			elapsedTimeMinutes = Long.toString(timer.getElapsedTimeSecs() / 60);
-			elapsedTimeHours = Long.toString(timer.getElapsedTimeSecs() / 3600);
-			elapsedTimeDays = Long.toString(timer.getElapsedTimeSecs() / 3600 / 24);
-			timerField.setText(elapsedTimeDays + " dni " + elapsedTimeHours + " godzin " + elapsedTimeMinutes + " min. "
-					+ elapsedTimeSecs + " s.");
+			stringTime(timer);
 
 		}
 
@@ -127,6 +156,96 @@ public class StopWatchApp extends JFrame
 
 		}
 
+	}
+
+	private class SaveItemListener implements ActionListener
+	{
+
+		@Override
+		public void actionPerformed(ActionEvent arg0)
+		{
+			
+			timer.stop();
+				
+			JFileChooser winChooser = new JFileChooser();
+			winChooser.setDialogTitle("Gdzie zapisaæ projekt?");
+			winChooser.showSaveDialog(getParent());
+
+			try
+			{
+				FileOutputStream outputStr = new FileOutputStream(winChooser.getSelectedFile());
+				ObjectOutputStream os = new ObjectOutputStream(outputStr);
+				os.writeObject(timer);
+				os.close();
+			} catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+
+	private class LoadItemListener implements ActionListener
+	{
+
+		@Override
+		public void actionPerformed(ActionEvent arg0)
+		{
+			{
+				
+				
+				timer.stop();
+				
+				StopWatch loadTimer = null;
+				JFileChooser fileCh = new JFileChooser();
+				fileCh.setDialogTitle("Który projekt odczytaæ?");
+				int veryfyCh = fileCh.showOpenDialog(getParent());
+
+				try
+				{
+					FileInputStream inputStr = new FileInputStream(fileCh.getSelectedFile());
+					ObjectInputStream objInputStr = new ObjectInputStream(inputStr);
+					loadTimer = (StopWatch) objInputStr.readObject();
+
+				} catch (Exception e)
+				{
+					e.printStackTrace();
+				} 
+				
+				timer.setStartTime(loadTimer.getStartTime());
+				timer.setStopTime(loadTimer.getStopTime());
+				timer.setResumeTime(loadTimer.getResumeTime());
+				timer.setRunning(loadTimer.isRunning());
+				stringTime(loadTimer);
+
+			}
+
+		}
+
+	}
+
+	private class ExitItemListener implements ActionListener
+	{
+
+		@Override
+		public void actionPerformed(ActionEvent arg0)
+		{
+			System.exit(0);
+
+		}
+
+	}
+
+	private void stringTime(StopWatch timer)
+	{
+		String elapsedTime = Long.toString(timer.getElapsedTimeSecs());
+		elapsedTimeSecs = Long.toString(timer.getElapsedTimeSecs() % 60);
+		elapsedTimeMinutes = Long.toString(timer.getElapsedTimeSecs() / 60);
+		elapsedTimeHours = Long.toString(timer.getElapsedTimeSecs() / 3600);
+		elapsedTimeDays = Long.toString(timer.getElapsedTimeSecs() / 3600 / 24);
+		timerField.setText(elapsedTimeDays + " dni " + elapsedTimeHours + " godzin " + elapsedTimeMinutes + " min. "
+				+ elapsedTimeSecs + " s.");
 	}
 
 }
